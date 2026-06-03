5fe6c5ee7cb39ef33f2ca30dacfb1b84a1649101
Radha <gaganjadon888@gmail.com>
2026-06-04 00:31:59 +0530

    fix api error

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://spendwise-expense-tracker-9ful.onrender.com"
+  baseURL: "https://spendwise-expense-tracker-9ful.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
